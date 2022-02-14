1412e788fa1767123e655ae2e3b7f030d31e5701
Vegar Ringdal <vegar.ringdal@gmail.com>
2022-02-14 21:32:05 +0100

    chore: add simple router with pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Page1Component } from './routes/page1/page1.component';
+import { Page2Component } from './routes/page2/page2.component';
+import { Page3Component } from './routes/page3/page3.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'page1',
+    component: Page1Component,
+  },
+  {
+    path: 'page2',
+    component: Page2Component,
+  },
+  {
+    path: 'page3',
+    component: Page3Component,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { RootComponent } from './root/root.component';
+import { Page1Component } from './routes/page1/page1.component';
+import { Page2Component } from './routes/page2/page2.component';
+import { Page3Component } from './routes/page3/page3.component';
+import { MainNavComponent } from './component/main-nav/main-nav.component';
 
 @NgModule({
-  declarations: [RootComponent],
+  declarations: [RootComponent, Page1Component, Page2Component, Page3Component, MainNavComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [RootComponent],
